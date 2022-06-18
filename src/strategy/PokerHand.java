c129e8ab12f3ac049e591b5aacc393404288d6f9
tsmiranda <tiago.s.miranda@sapo.pt>
2022-06-18 17:21:38 +0100

    Alterações

--- src/strategy/PokerHand.java
+++ src/strategy/PokerHand.java
@@ -66,6 +66,78 @@ public enum PokerHand {
 		}
 		
 	},
+	
+	FOUR_TO_ROYAL_FLUSH {
+		
+		@Override
+		public
+		boolean matches(List<Card> hand) {
+
+			return Utils.isFourOfAKind("FOUR_2_4", hand);
+
+		}
+		
+	},
+	
+	THREE_ACES {
+		
+		@Override
+		public
+		boolean matches(List<Card> hand) {
+
+			return Utils.isThreeAces(hand);
+
+		}
+		
+	},
+	
+	STRAIGHT {
+		
+		@Override
+		public
+		boolean matches(List<Card> hand) {
+
+//	    	int firstCardSuit = playerHand.get(0).getSuit();
+	    	List<Integer> sortedCardRanks = new ArrayList<>();
+	    	List<Integer> cardSuits = new ArrayList<>();
+
+	    	//Create an Integer list containing all the player's ranks
+	    	for(Card card : hand){
+	    		sortedCardRanks.add(card.getRank().getValue());
+	    		cardSuits.add(card.getSuit().getValue());
+	    	}
+
+	    	//Sort the ranks
+	    	Collections.sort(sortedCardRanks);
+
+	    	//Check to see that all card suits are not identical
+	    	Set<Integer> suitSet = new HashSet<>(cardSuits);
+
+	    	//If set size is smaller, there are duplicates, meaning more than one suit, which a Straight requires
+	    	if(suitSet.size() > cardSuits.size())
+	    		return false;
+
+	    	//Go Step by step to see if the next card's rank is only 1 more than it
+	    	for(int i = 0; i < 4; i++){
+	    		if(!(sortedCardRanks.get(i) == (sortedCardRanks.get(i+1) - 1)))
+	    			return false;
+	    	}
+
+	    	return true;
+		}
+	},
+	
+	FLUSH {
+		
+		@Override
+		public
+		boolean matches(List<Card> hand) {
+
+			return Utils.isFlush("FLUSH", hand);
+		}
+	},
+	
+	
 
 		FULL_HOUSE {
 		
@@ -77,51 +149,6 @@ public enum PokerHand {
 			}
 		},
 
-		FLUSH {
-			
-			@Override
-			public
-			boolean matches(List<Card> hand) {
-
-				return Utils.isFlush("FLUSH", hand);
-			}
-		},
-
-		STRAIGHT {
-			
-			@Override
-			public
-			boolean matches(List<Card> hand) {
-
-//		    	int firstCardSuit = playerHand.get(0).getSuit();
-		    	List<Integer> sortedCardRanks = new ArrayList<>();
-		    	List<Integer> cardSuits = new ArrayList<>();
-
-		    	//Create an Integer list containing all the player's ranks
-		    	for(Card card : hand){
-		    		sortedCardRanks.add(card.getRank().getValue());
-		    		cardSuits.add(card.getSuit().getValue());
-		    	}
-
-		    	//Sort the ranks
-		    	Collections.sort(sortedCardRanks);
-
-		    	//Check to see that all card suits are not identical
-		    	Set<Integer> suitSet = new HashSet<>(cardSuits);
-
-		    	//If set size is smaller, there are duplicates, meaning more than one suit, which a Straight requires
-		    	if(suitSet.size() > cardSuits.size())
-		    		return false;
-
-		    	//Go Step by step to see if the next card's rank is only 1 more than it
-		    	for(int i = 0; i < 4; i++){
-		    		if(!(sortedCardRanks.get(i) == (sortedCardRanks.get(i+1) - 1)))
-		    			return false;
-		    	}
-
-		    	return true;
-			}
-		},
 
 		THREE_OF_A_KIND {
 
@@ -132,6 +159,19 @@ public enum PokerHand {
 				return Utils.isThreeOfAKind(hand);
 			}
 
+		},
+		
+		//To advice
+		FOUR_TO_A_STRAIGHT_FLUSH {
+			
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+
+				return Utils.isFourToAStraigthFlush(hand);
+
+			}
+			
 		},
 
 		TWO_PAIR {
@@ -144,7 +184,7 @@ public enum PokerHand {
 			}
 
 		},
-
+		
 		JACKS_OR_BETTER {
 
 			@Override
@@ -163,6 +203,308 @@ public enum PokerHand {
 			
 
 		},
+		
+		//To advice
+		HIGH_PAIR {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isHighPair(hand);
+			}
+
+		},
+		//To advice
+		FOUR_TO_A_FLUSH {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isFourToAFlush(hand);
+			}
+
+		},
+		//To advice
+		THREE_TO_A_ROYAL_FLUSH {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isThreeToARoyalFlush(hand);
+			}
+
+		},
+		//To advice
+		FOUR_TO_AN_OUTSIDE_STRAIGHT {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isFourToAnOutsideStraigth(hand);
+			}
+
+		},
+		//To advice
+		LOW_PAIR {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isLowPair(hand);
+			}
+
+		},
+		//To advice
+		AKQJ_UNSUITED {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isAKQJUnsuited(hand);
+			}
+
+		},
+		//To advice
+		THREE_TO_A_STRAIGHT_FLUSH_TYPE_1 {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isThreeToAStraightFlush(1,hand);
+			}
+
+		},
+		
+		FOUR_TO_AN_INSIDE_STRAIGHT_3_HIGH_CARDS {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isFourToAnInsideStraight(3,hand);
+			}
+
+		},
+		
+		QJ_SUITED {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isQJSuited(hand);
+			}
+
+		},
+		
+		THREE_TO_A_FLUSH_WITH_2_HIGH_CARDS  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isThreeToAFlush(2,hand);
+			}
+
+		},
+		
+		TWO_SUITED_HIGH_CARDS  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isTwoSuitedHighCards(hand);
+			}
+
+		},
+		
+		FOUR_TO_AN_INSIDE_STRAIGHT_WITH_TWO_HIGH_CARDS  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isFourToAnInsideStraight(2,hand);
+			}
+
+		},
+		
+		THREE_TO_A_STRAIGHT_FLUSH_TYPE_2  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isThreeToAStraightFlush(2,hand);
+			}
+
+		},
+		
+		FOUR_TO_AN_INSIDE_STRAIGHT_WITH_ONE_HIGH_CARD  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isFourToAnInsideStraight(1,hand);
+			}
+
+		},
+		
+		 KQJ_UNSUITED  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isKQJUnsuited(hand);
+			}
+
+		},
+		 
+		 JT_SUITED  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isJTSuited(hand);
+			}
+
+		},
+		 
+		 QJ_UNSUITED  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isQJUnsuited(hand);
+			}
+
+		},
+		 
+		 THREE_TO_A_FLUSH_WITH_1_HIGH_CARD  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isThreeToAFlush(1,hand);
+			}
+
+		},
+		 
+		 QT_SUITED  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isQTSuited(hand);
+			}
+
+		},
+		 
+		 THREE_TO_A_STRAIGHT_FLUSH_TYPE_3  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isThreeToAStraightFlush(3,hand);
+			}
+
+		},
+		 
+		 KQ_KJ_UNSUITED  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isKQOrKJUnsuited(hand);
+			}
+
+		},
+		 
+		 ACE  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isAce(hand);
+			}
+
+		},
+		 
+		 KT_SUITED  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isKTSuited(hand);
+			}
+
+		},
+		 
+		 JACK_QUEEN_KING  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isJQK(hand);
+			}
+
+		},
+		 
+		 FOUR_TO_AN_INSIDE_STRAIGHT_WITH_NO_HIGH_CARDS  {
+
+			@Override
+			public
+			boolean matches(List<Card> hand) {
+				// TODO Auto-generated method stub
+				return Utils.isFourToAnInsideStraight(0,hand);
+			}
+
+		},
+		 
+		 THREE_TO_A_FLUSH_WITH_NO_HIGH_CARDS  {
+
+				@Override
+				public
+				boolean matches(List<Card> hand) {
+					// TODO Auto-generated method stub
+					return Utils.isThreeToAFlush(0,hand);
+				}
+
+			},
+		 
+		 DISCARD_EVERYTHING  {
+
+				@Override
+				public
+				boolean matches(List<Card> hand) {
+					// TODO Auto-generated method stub
+					return Utils.isDiscardEverything(hand);
+				}
+
+			},
+		 
+		 
+
+		
 		NONE {
 			
 			@Override
--- src/strategy/Utils.java
+++ src/strategy/Utils.java
@@ -2,8 +2,11 @@ package strategy;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.Map.Entry;
 
 import card.Card;
@@ -57,6 +60,83 @@ public static boolean isFullHouse(List<Card> hand) {
 
 	return map.containsValue(3) && map.containsValue(2);
 }
+
+
+public static boolean isStraight(List<Card> hand) {
+	
+
+//	int firstCardSuit = playerHand.get(0).getSuit();
+	List<Integer> sortedCardRanks = new ArrayList<>();
+	List<Integer> cardSuits = new ArrayList<>();
+
+	//Create an Integer list containing all the player's ranks
+	for(Card card : hand){
+		sortedCardRanks.add(card.getRank().getValue());
+		cardSuits.add(card.getSuit().getValue());
+	}
+
+	//Sort the ranks
+	Collections.sort(sortedCardRanks);
+
+	//Check to see that all card suits are not identical
+	Set<Integer> suitSet = new HashSet<>(cardSuits);
+
+	//If set size is smaller, there are duplicates, meaning more than one suit, which a Straight requires
+	if(suitSet.size() > cardSuits.size())
+		return false;
+
+	//Go Step by step to see if the next card's rank is only 1 more than it
+	for(int i = 0; i < 4; i++){
+		if(!(sortedCardRanks.get(i) == (sortedCardRanks.get(i+1) - 1)))
+			return false;
+	}
+
+	return true;
+}
+
+public static List<Integer> straight(List<Card> hand) {
+	
+
+//	int firstCardSuit = playerHand.get(0).getSuit();
+	List<Integer> sortedCardRanks = new ArrayList<>();
+	List<Integer> cardSuits = new ArrayList<>();
+	List<Integer> positionList = new ArrayList<>();
+	int count = 0;
+	int pos = 0;
+
+	//Create an Integer list containing all the player's ranks
+	for(Card card : hand){
+		sortedCardRanks.add(card.getRank().getValue());
+		cardSuits.add(card.getSuit().getValue());
+	}
+
+	//Sort the ranks
+	Collections.sort(sortedCardRanks);
+
+	//Check to see that all card suits are not identical
+	Set<Integer> suitSet = new HashSet<>(cardSuits);
+
+	//If set size is smaller, there are duplicates, meaning more than one suit, which a Straight requires
+	if(suitSet.size() > cardSuits.size())
+		return null;
+
+	//Go Step by step to see if the next card's rank is only 1 more than it
+	for(int i = 0; i < 4; i++){
+		
+		if(!(sortedCardRanks.get(i) == (sortedCardRanks.get(i+1) - 1)))
+			count++;
+	}
+	
+	if(count==5) {
+		for(int i=0;i<5;i++) {
+			positionList.add(i);
+		}
+	}
+		return null;
+	
+}
+	
+
 
 public static boolean isFlush(String type, List<Card> hand) {
 	
@@ -162,25 +242,362 @@ public static List<Integer> fourToRoyalFlush(List<Card> hand){
 	
 	for(Card card : hand){
 			
-		if(list.contains(card.getRank().getValue()))
+		if(list.contains(card.getRank().getValue())) {
 			if(suit==0) {
 				suit=card.getSuit().getValue();
 				positionList.add(position);
 			} else if(card.getSuit().getValue()==suit) {
 				positionList.add(position);
+				position++;
 			}
-		position++;
+		
+	}
 	}
 	
-	if(positionList.size()<3) {
-		positionList.clear();
-		return positionList;
+	if(positionList.size()<4) {	
+		return null;
 	}
 	
 	return positionList;
 	
 	
 }
+
+
+public static boolean isFourToRoyalFlush(List<Card> hand){
+	
+	List<Integer> list = Arrays.asList(1,10,11,12,13);
+	
+	int suit = 0;
+	
+	int position=0;
+	
+	
+	for(Card card : hand){
+			
+		if(list.contains(card.getRank().getValue())) {
+			if(suit==0) {
+				suit=card.getSuit().getValue();
+			} else if(card.getSuit().getValue()==suit) {
+				position++;
+			}
+		
+	}
+	}
+	
+	if(position==4) {
+		return true;
+	}
+	
+	return false;
+	
+	
+}
+
+
+public static boolean isThreeAces(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isFourToAStraigthFlush(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isHighPair(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isFourToAFlush(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isThreeToARoyalFlush(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isFourToAnOutsideStraigth(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isLowPair(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isAKQJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isThreeToAStraightFlush(int type,List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+
+
+public static boolean isFourToAnInsideStraight(int type,List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isQJSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isThreeToAFlush(int type,List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isTwoSuitedHighCards(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isKQJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isJTSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isQJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isQTSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isKQOrKJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isAce(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isKTSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isJQK(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static boolean isDiscardEverything(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return false;
+}
+
+
+public static List<Integer> discardEverything(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> threeToAFlush(int i, List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> fourToAnInsideStraight(int i, List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> JQK(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> KTSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> Ace(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> KQOrKJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> threeToAStraightFlush(int i, List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> QTSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> JTSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> QJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> KQJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> twoSuitedHighCards(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> QJSuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> AKQJUnsuited(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> lowPair(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> fourToAnOutsideStraigth(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> threeToARoyalFlush(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> fourToAFlush(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> highPair(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> jacksOrBetter(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> twoPair(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> fourToAStraigthFlush(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> threeOfAKind(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> fullHouse(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> flush(String string, List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> threeAces(List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+public static List<Integer> fourOfAKind(String string, List<Card> hand) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+
+
 
 
 //public boolean isFourToRoyalFlush(List<Card> hand){
